d974a27b55e7adb4bc17deb11ff922c5508c561f
Martin Beneder <47112278+lordLegal@users.noreply.github.com>
2023-08-28 23:53:38 +0200

    Update next.config.js

--- spotify-analyse/next.config.js
+++ spotify-analyse/next.config.js
@@ -14,12 +14,14 @@ const nextConfig = {
         }]
     },
     async rewrites() {
-        return [
-            {
-                source: '/api/trade/:path*',
-                destination: 'http://127.0.0.1:8000/:path*',
-            },
-        ]
+        return {
+            afterFiles: [
+                {
+                    source: '/api/trade/:path*',
+                    destination: 'http://127.0.0.1:8000/:path*',
+                },
+            ]
+        }
     }
 }
 
