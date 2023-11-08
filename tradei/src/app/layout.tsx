d79196f2d2c22a700ce7e89add70d67c7ad48087
Martin Beneder <47112278+lordLegal@users.noreply.github.com>
2023-11-08 15:17:58 +0000

    update style

--- tradei/src/app/layout.tsx
+++ tradei/src/app/layout.tsx
@@ -8,7 +8,7 @@ import {FaSpotify} from 'react-icons/fa'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Trade Spotify',
+  title: 'Tradei',
   description: 'Analyse and Trade with Coins using your Spotify Data',
 }
 
@@ -19,9 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Nav></Nav><div className='pt-20'>{children}</div><footer className="flex flex-col justify-center w-full h-24 border-t">
+      <body className={inter.className}><Nav></Nav><div className='pt-20'>{children}</div><footer className="flex justify-center w-full h-24 border-t">
           <div>Coded and Designed by <Link href="https://github.com/lordLegal" className="text-green-600"> Martin Beneder</Link></div>
-          <div className='flex flex-col items-center text-center'>
+          <div className='flex flex-col'>
           <Link href="https://spotify.com/"><FaSpotify className="fill-green-500" ></FaSpotify>View it on Spotify</Link>
           </div>
         </footer></body>
